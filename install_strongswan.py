43494b250ef70b4d79a7c386af3757ddf9b431f5
Diego Johnson <38963618+blackout9@users.noreply.github.com>
2023-10-25 01:10:36 -0700

    Update install_strongswan.py

--- install_strongswan.py
+++ install_strongswan.py
@@ -1,25 +1,29 @@
-"""This script installs StrongSwan on the Rasberry-Pi."""
+"""This script installs StrongSwan on the Raspberry Pi."""
 
-# Import the subprocess module
+# Imports the subprocess module
 import subprocess
 
-# Uses the 'subprocess' import to run commands through the shell
-def run_command(command):
-    """Run a shell command."""
-    try:
-        result = subprocess.run(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True
-        )
-        return result.stdout.decode("utf-8")
-    except subprocess.CalledProcessError as e:
-        print(f"Command failed with error: {e}")
-        return e.stderr.decode("utf-8")
+# Define a function to run shell commands
+def run_command(command: str) -> str:
+  """Run a shell command and return the output as a string."""
+  try:
+    result = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True
+    )
+    return result.stdout.decode("utf-8")
+  except subprocess.CalledProcessError as e:
+    print(f"Command failed with error: {e}")
+    return e.stderr.decode("utf-8")
 
-# Installs StrongSwan and related packages
+# Define a function to install StrongSwan
 def install_strongswan():
-    """Install StrongSwan and related packages."""
-    run_command("sudo apt update")
-    run_command("sudo apt install -y strongswan strongswan-pki")
+  """Install StrongSwan and related packages."""
+  run_command("sudo apt update")
+  run_command("sudo apt install -y strongswan strongswan-pki")
 
 # Execute the function
-install_strongswan()
+def main():
+  install_strongswan()
+
+if __name__ == '__main__':
+  main()
